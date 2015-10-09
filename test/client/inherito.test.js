91a9475aa006d3d80f478897c7e7919ea978749a
Oz <oscar@touchtribe.nl>
2015-10-09 10:22:01 +0200

    included travis ci

--- test/client/inherito.test.js
+++ test/client/inherito.test.js
@@ -1,9 +1,34 @@
 var assert = require("assert");
+
 describe('Array', function() {
+  
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+  
+});
+
+describe('Inherito', () => {
+   describe('create', () => {
+     // Process how object is being created and placed inside other object as prop
+     // Check if object is not iterable
+     // Check if object is hidden
+     assert.Object.equal(Object.isExtensible(this));
+     
+   });
+   
+   describe('inherit', () => {
+     // Check wether give arguments are being passed correctly to the object
+     // is prototype of parent object and is inheriting just the given objects
+      
+   });
+   
+   // in progress do not test yet
+   describe('view', () => {
+     // Combine react core and react-dom for views
+     
+   });
 });
